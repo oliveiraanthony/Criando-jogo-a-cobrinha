889cb9fbf4a0d39d008add059536a187feb62d3f
oliveiraanthony <oliveira.anthony@gmail.com>
2021-02-27 10:09:54 -0300

    definir movimento da cobrinha e impedir que ela suma da tela

--- js/script.js
+++ js/script.js
@@ -19,11 +19,23 @@ function criarCobrinha(){
             context.fillStyle = "red"
             context.fillRect(snake[i].x, snake[i].y, box, box);
         }
+}
 
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
 
 }
 
 function iniciarJogo(){
+        if(snake[0].x > 15 && direction == "right") snake[0].x = 0;
+        if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+        if(snake[0].y > 15 && direction == "down") snake[0].y = 0;
+        if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
     criarBG();
     criarCobrinha()
 
@@ -43,9 +55,6 @@ function iniciarJogo(){
     }
 
     snake.unshift(newHead);
-
-
-
 }
 
 let jogo = setInterval(iniciarJogo, 100);
