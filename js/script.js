5a707726f3f72dc7c18a310d25b1c1e11ecb7ef3
oliveiraanthony <oliveira.anthony@gmail.com>
2021-02-27 10:22:17 -0300

    comida adicionada na tela

--- js/script.js
+++ js/script.js
@@ -8,6 +8,10 @@ snake[0] = {
 }
 
 let direction = "right"
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG(){
     context.fillStyle = "lightgreen";
@@ -21,6 +25,12 @@ function criarCobrinha(){
         }
 }
 
+function drawFood(){
+    context.fillStyle= "green";
+    context.fillRect(food.x, food.y, box, box);
+
+}
+
 document.addEventListener('keydown', update);
 
 function update(event){
@@ -32,12 +42,13 @@ function update(event){
 }
 
 function iniciarJogo(){
-        if(snake[0].x > 15 && direction == "right") snake[0].x = 0;
+        if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
         if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-        if(snake[0].y > 15 && direction == "down") snake[0].y = 0;
+        if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
         if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
     criarBG();
-    criarCobrinha()
+    criarCobrinha();
+    drawFood();
 
     let snakex = snake[0].x;
     let snakey = snake[0].y;
